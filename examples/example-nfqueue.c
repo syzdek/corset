54975815f58ff47a24edf62c79484f535b1eef92
David M. Syzdek <david@syzdek.net>
2023-02-03 15:04:53 -0900

    example-nfqueue: cleaning up whitespace

--- examples/example-nfqueue.c
+++ examples/example-nfqueue.c
@@ -60,9 +60,7 @@
 //  Headers  //
 //           //
 ///////////////
-#ifdef __CORSET_PMARK
 #pragma mark - Headers
-#endif
 
 #include <errno.h>
 #include <stdio.h>
@@ -80,28 +78,31 @@
 #include <linux/ipv6.h>
 
 
-///////////////////
-//               //
-//  Definitions  //
-//               //
-///////////////////
-#ifdef __CORSET_PMARK
-#pragma mark - Definitions
-#endif
-
-
 //////////////////
 //              //
 //  Prototypes  //
 //              //
 //////////////////
-#ifdef __CORSET_PMARK
 #pragma mark - Prototypes
-#endif
 
-void my_signal(int sig);
-int  my_callback(struct nfq_q_handle * qh, struct nfgenmsg * nfmsg, struct nfq_data * nfad, void * data);
-int  main(int argc, char * argv[]);
+extern int
+main(
+         int                           argc,
+         char *                        argv[] );
+
+
+static int
+my_callback(
+         struct nfq_q_handle *         qh,
+         struct nfgenmsg *             nfmsg,
+         struct nfq_data *             nfad,
+         void *                        data );
+
+
+static void
+my_signal(
+         int                           sig );
+
 
 
 /////////////////
@@ -128,166 +129,13 @@ char       * prog_name       = NULL;
 #pragma mark - Functions
 #endif
 
-void my_signal(int sig)
-{
-   signal(sig, my_signal);
-   switch(sig)
-   {
-      case SIGUSR1:
-      if (verdict_current != NF_ACCEPT)
-      {
-         verdict_string  = "ACCEPT";
-         verdict_current = NF_ACCEPT;
-         printf("%s changing policy to \"%s\"\n", prog_name, verdict_string);
-      };
-      break;
-
-      case SIGUSR2:
-      if (verdict_current != NF_DROP)
-      {
-         verdict_string  = "DROP";
-         verdict_current = NF_DROP;
-         printf("%s changing policy to \"%s\"\n", prog_name, verdict_string);
-      };
-      break;
-
-      case SIGHUP:
-      verdict_string  = (verdict_current == NF_ACCEPT) ? "DROP"  : "ACCEPT";
-      verdict_current = (verdict_current == NF_ACCEPT) ? NF_DROP : NF_ACCEPT;
-      printf("%s changing policy to \"%s\"\n", prog_name, verdict_string);
-      break;
-
-      case SIGINT:
-      case SIGQUIT:
-      case SIGTERM:
-      printf("%s exiting\n", prog_name);
-      should_exit = 1;
-      break;
-
-      default:
-      break;
-   };
-   return;
-}
-
-
-int my_callback(struct nfq_q_handle * qh, struct nfgenmsg * nfmsg, struct nfq_data * nfad, void * data)
-{
-   int                             id;
-   struct nfqnl_msg_packet_hdr   * ph;
-   uint16_t                        hwpos;
-   int                             payload_len;
-   unsigned char                 * payload;
-   struct iphdr                  * ip4;
-   struct ipv6hdr                * ip6;
-   char                            addr_src[INET6_ADDRSTRLEN];
-   char                            addr_dst[INET6_ADDRSTRLEN];
-   char                            addr_hw[INET6_ADDRSTRLEN];
-   uint16_t                        port_src;
-   uint16_t                        port_dst;
-   size_t                          offset;
-   const char                    * proto;
-   struct nfqnl_msg_packet_hw    * packet_hw;
-
-   assert(data != NULL);
-
-
-   // obtain packet information
-   if ((ph = nfq_get_msg_packet_hdr(nfad)) == NULL)
-      return(0);
-   id = ntohl(ph->packet_id);
-   if ((payload_len = nfq_get_payload(nfad, &payload)) == -1)
-      return(nfq_set_verdict(qh, id, NF_DROP, 0, NULL));
-   if ((packet_hw = nfq_get_packet_hw(nfad)) == NULL)
-      return(nfq_set_verdict(qh, id, NF_DROP, 0, NULL));
-
-
-   // process hardware address
-   for(hwpos = 0; (hwpos < ntohs(packet_hw->hw_addrlen)); hwpos++)
-   {
-      snprintf( &addr_hw[hwpos*3],
-               sizeof(addr_hw)-(hwpos*3),
-               "%02x-",
-               packet_hw->hw_addr[hwpos] );
-   };
-   addr_hw[((hwpos)) ? (hwpos*3)-1 : 0] = '\0';
-
-
-   // process IP addresses and port
-   ip4       = ((struct iphdr *)   payload);
-   ip6       = ((struct ipv6hdr *) payload);
-   port_src  = 0;
-   port_dst  = 0;
-   switch(ip4->version)
-   {
-      case 4:
-      inet_ntop(AF_INET, &ip4->saddr, addr_src, sizeof(addr_src));
-      inet_ntop(AF_INET, &ip4->daddr, addr_dst, sizeof(addr_dst));
-      if ((ip4->protocol == 6) || (ip4->protocol == 17))
-      {
-         offset = ip4->ihl * 4;
-         port_src = ntohs( *((short *)&payload[offset+0]) );
-         port_dst = ntohs( *((short *)&payload[offset+2]) );
-      };
-      break;
-
-      case 6:
-      inet_ntop(AF_INET6, &ip6->saddr, addr_src, sizeof(addr_src));
-      inet_ntop(AF_INET6, &ip6->daddr, addr_dst, sizeof(addr_dst));
-      if ((ip6->nexthdr == 6) || (ip6->nexthdr == 17))
-      {
-         offset = 40;
-         port_src = ntohs( *((short *)&payload[offset+0]) );
-         port_dst = ntohs( *((short *)&payload[offset+2]) );
-      };
-      break;
-
-      default:
-      break;
-   };
-
-
-   // determine protocol name (not full list)
-   switch((ip4->version == 4) ? ip4->protocol : ip6->nexthdr)
-   {
-      case 1:  proto = "ICMP";       break;
-      case 6:  proto = "TCP";        break;
-      case 17: proto = "UDP";        break;
-      case 58: proto = "IPv6-ICMP";  break;
-      default: proto = "OTHER";      break;
-   };
-
-
-   // print packet information
-   if ( ((port_src)) || ((port_dst)) )
-      printf( "%s (%i) %s hw=%s src=%s dst=%s proto=%s spt=%hu dpt=%hu\n",
-              prog_name, id,
-              verdict_string,
-              addr_hw,
-              addr_src, addr_dst,
-              proto,
-              port_src, port_dst );
-   else
-      printf( "%s (%i) %s hw=%s src=%s dst=%s proto=%s\n",
-              prog_name, id,
-              verdict_string,
-              addr_hw,
-              addr_src, addr_dst,
-              proto );
-   fflush(stdout);
-
-
-   return(nfq_set_verdict(qh, id, verdict_current, 0, NULL));
-}
-
-
 int main(int argc, char * argv[])
 {
    int                     queue_num;
    int                     rc;
    int                     fd;
-   struct nfq_handle     * nh;
-   struct nfq_q_handle   * qh;
+   struct nfq_handle *     nh;
+   struct nfq_q_handle *   qh;
    char                    msg[0xffff];
    ssize_t                 msglen;
 
@@ -400,4 +248,163 @@ int main(int argc, char * argv[])
    return(0);
 }
 
+int
+my_callback(
+         struct nfq_q_handle *         qh,
+         struct nfgenmsg *             nfmsg,
+         struct nfq_data *             nfad,
+         void *                        data )
+{
+   int                              id;
+   struct nfqnl_msg_packet_hdr *    ph;
+   uint16_t                         hwpos;
+   int                              payload_len;
+   unsigned char *                  payload;
+   struct iphdr *                   ip4;
+   struct ipv6hdr *                 ip6;
+   char                             addr_src[INET6_ADDRSTRLEN];
+   char                             addr_dst[INET6_ADDRSTRLEN];
+   char                             addr_hw[INET6_ADDRSTRLEN];
+   uint16_t                         port_src;
+   uint16_t                         port_dst;
+   size_t                           offset;
+   const char *                     proto;
+   struct nfqnl_msg_packet_hw *     packet_hw;
 
+   assert(data != NULL);
+
+
+   // obtain packet information
+   if ((ph = nfq_get_msg_packet_hdr(nfad)) == NULL)
+      return(0);
+   id = ntohl(ph->packet_id);
+   if ((payload_len = nfq_get_payload(nfad, &payload)) == -1)
+      return(nfq_set_verdict(qh, id, NF_DROP, 0, NULL));
+   if ((packet_hw = nfq_get_packet_hw(nfad)) == NULL)
+      return(nfq_set_verdict(qh, id, NF_DROP, 0, NULL));
+
+
+   // process hardware address
+   for(hwpos = 0; (hwpos < ntohs(packet_hw->hw_addrlen)); hwpos++)
+   {
+      snprintf( &addr_hw[hwpos*3],
+               sizeof(addr_hw)-(hwpos*3),
+               "%02x-",
+               packet_hw->hw_addr[hwpos] );
+   };
+   addr_hw[((hwpos)) ? (hwpos*3)-1 : 0] = '\0';
+
+
+   // process IP addresses and port
+   ip4       = ((struct iphdr *)   payload);
+   ip6       = ((struct ipv6hdr *) payload);
+   port_src  = 0;
+   port_dst  = 0;
+   switch(ip4->version)
+   {
+      case 4:
+      inet_ntop(AF_INET, &ip4->saddr, addr_src, sizeof(addr_src));
+      inet_ntop(AF_INET, &ip4->daddr, addr_dst, sizeof(addr_dst));
+      if ((ip4->protocol == 6) || (ip4->protocol == 17))
+      {
+         offset = ip4->ihl * 4;
+         port_src = ntohs( *((short *)&payload[offset+0]) );
+         port_dst = ntohs( *((short *)&payload[offset+2]) );
+      };
+      break;
+
+      case 6:
+      inet_ntop(AF_INET6, &ip6->saddr, addr_src, sizeof(addr_src));
+      inet_ntop(AF_INET6, &ip6->daddr, addr_dst, sizeof(addr_dst));
+      if ((ip6->nexthdr == 6) || (ip6->nexthdr == 17))
+      {
+         offset = 40;
+         port_src = ntohs( *((short *)&payload[offset+0]) );
+         port_dst = ntohs( *((short *)&payload[offset+2]) );
+      };
+      break;
+
+      default:
+      break;
+   };
+
+
+   // determine protocol name (not full list)
+   switch((ip4->version == 4) ? ip4->protocol : ip6->nexthdr)
+   {
+      case 1:  proto = "ICMP";       break;
+      case 6:  proto = "TCP";        break;
+      case 17: proto = "UDP";        break;
+      case 58: proto = "IPv6-ICMP";  break;
+      default: proto = "OTHER";      break;
+   };
+
+
+   // print packet information
+   if ( ((port_src)) || ((port_dst)) )
+      printf( "%s (%i) %s hw=%s src=%s dst=%s proto=%s spt=%hu dpt=%hu\n",
+              prog_name, id,
+              verdict_string,
+              addr_hw,
+              addr_src, addr_dst,
+              proto,
+              port_src, port_dst );
+   else
+      printf( "%s (%i) %s hw=%s src=%s dst=%s proto=%s\n",
+              prog_name, id,
+              verdict_string,
+              addr_hw,
+              addr_src, addr_dst,
+              proto );
+   fflush(stdout);
+
+
+   return(nfq_set_verdict(qh, id, verdict_current, 0, NULL));
+}
+
+
+void
+my_signal(
+         int                           sig )
+{
+   signal(sig, my_signal);
+   switch(sig)
+   {
+      case SIGUSR1:
+      if (verdict_current != NF_ACCEPT)
+      {
+         verdict_string  = "ACCEPT";
+         verdict_current = NF_ACCEPT;
+         printf("%s changing policy to \"%s\"\n", prog_name, verdict_string);
+      };
+      break;
+
+      case SIGUSR2:
+      if (verdict_current != NF_DROP)
+      {
+         verdict_string  = "DROP";
+         verdict_current = NF_DROP;
+         printf("%s changing policy to \"%s\"\n", prog_name, verdict_string);
+      };
+      break;
+
+      case SIGHUP:
+      verdict_string  = (verdict_current == NF_ACCEPT) ? "DROP"  : "ACCEPT";
+      verdict_current = (verdict_current == NF_ACCEPT) ? NF_DROP : NF_ACCEPT;
+      printf("%s changing policy to \"%s\"\n", prog_name, verdict_string);
+      break;
+
+      case SIGINT:
+      case SIGQUIT:
+      case SIGTERM:
+      printf("%s exiting\n", prog_name);
+      should_exit = 1;
+      break;
+
+      default:
+      break;
+   };
+   return;
+}
+
+/* end of source */
