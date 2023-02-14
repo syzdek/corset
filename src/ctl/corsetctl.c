214e7e5f29300f4c9d24704d75c3b146c638556a
David M. Syzdek <david@syzdek.net>
2023-02-14 00:16:18 -0900

    corsetctl: adding program stub

---
+++ src/ctl/corsetctl.c
@@ -0,0 +1,149 @@
+
+/*
+ *  Corset Firewall
+ *  Copyright (C) 2023 David M. Syzdek <david@syzdek.net>.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of David M. Syzdek nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DAVID M SYZDEK BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ */
+#define _SRC_CORSETD_CORSETCTL_C 1
+#include "corsetctl.h"
+
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
+#pragma mark - Headers
+
+#include <errno.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdio.h>
+
+#include <corset.h>
+#include <bindle_prefix.h>
+
+
+///////////////////
+//               //
+//  Definitions  //
+//               //
+///////////////////
+#pragma mark - Definitions
+
+#ifndef PROGRAM_NAME
+#define PROGRAM_NAME "corsetctl"
+#endif
+#ifndef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "david@syzdek.net"
+#endif
+#ifndef PACKAGE_COPYRIGHT
+#define PACKAGE_COPYRIGHT ""
+#endif
+#ifndef PACKAGE_NAME
+#define PACKAGE_NAME ""
+#endif
+#ifndef PACKAGE_VERSION
+#define PACKAGE_VERSION ""
+#endif
+
+
+//////////////////
+//              //
+//  Prototypes  //
+//              //
+//////////////////
+#pragma mark - Prototypes
+
+extern int
+main(
+         int                           argc,
+         char *                        argv[] );
+
+
+/////////////////
+//             //
+//  Variables  //
+//             //
+/////////////////
+#pragma mark - Variables
+
+
+/////////////////
+//             //
+//  Functions  //
+//             //
+/////////////////
+#pragma mark - Functions
+
+int
+main(
+         int                           argc,
+         char *                        argv[] )
+{
+   int               rc;
+   corset_cnf_t *    cnf;
+
+   static const char *  short_opt = "fhL:qVv";
+
+   // initialize library
+   corset_prog_name(PROGRAM_NAME);
+   if ((cnf = corset_init()) == NULL)
+   {
+      fprintf(stderr, "%s: out of virtual memory\n", PROGRAM_NAME);
+      return(1);
+   };
+
+   if ((rc = corset_getopt(cnf, argc, argv, short_opt)) != 0)
+   {
+      corset_destroy(cnf);
+      return( (rc == 1) ? 1 : 0 );
+   };
+
+   corset_debug_config(cnf, NULL);
+
+   corset_destroy(cnf);
+
+   return(0);
+}
+
+
+void
+corset_usage(
+         const char *                  short_opt )
+{
+   printf("Usage: %s [OPTIONS]\n", corset_prog_name(NULL));
+   corset_usage_options(short_opt);
+   printf("\n");
+   return;
+}
+
+
+/* end of source file */
